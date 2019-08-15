27a94ab524f41d5079a7f3dc737d4421262a45b9
Faisal Umair <eff.umair@gmail.com>
2019-08-15 18:15:14 +0530

    Yoti test

--- server/index.js
+++ server/index.js
@@ -157,13 +157,15 @@ app.get('*', (req, res) => {
   }
 
   if (req.url.startsWith('/yoti-verified')) {
-    sharetribeSdk.currentUser.updateProfile({
-      privateData: {
-        idVerification: 'faisal'
-      }
-    }).then(){
-      return res.status(404).send('OK');
-    }
+    sharetribeSdk.currentUser
+      .updateProfile({
+        privateData: {
+          idVerification: 'faisal',
+        },
+      })
+      .then(function() {
+        return res.status(404).send('OK');
+      });
   }
 
   const context = {};
