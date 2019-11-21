481e6a386c07c23e85e7d0f6ecdf09a0cf7e7b14
Aleksa <aleksaprebiracevicorona@outlook.com>
2019-11-21 05:42:20 +0100

    side help content + word elipsis on listing card

--- src/components/SideHelp/SideHelp.js
+++ src/components/SideHelp/SideHelp.js
@@ -18,12 +18,12 @@ class SideHelp extends React.Component {
         services in your area, region or country.`,
       },
       {
-        title: 'Test 1 Title',
-        description: 'Test 1 Description',
+        title: 'How Do I Find Homes and Pets to Sit?',
+        description: 'You can browse Pet Owners who have placed a listing in area or country specific locations. Get started by clicking the link on the home page “Find Homes to Sit” and you will then be redirected to the Trust My Pet Sitter key search page.',
       },
       {
-        title: 'Test 2 Title',
-        description: 'Test 2 Description',
+        title: 'How are Pet Sitter and Owners verified?',
+        description: 'All Pet Sitters and Pet Owners are encouraged to prove ID verification, offering all our users more peace of mind.',
       },
     ],
   };
@@ -134,8 +134,86 @@ class SideHelp extends React.Component {
           </li>
         </ul>
       </div>,
-      <div>Test 1</div>,
-      <div>Test 2</div>,
+      <div>
+        <h3 className={css.insideTitle}>How Do I Find Homes and Pets to Sit?</h3>
+        <p className={css.insideP}>
+          You can browse Pet Owners who have placed a listing in area or country specific locations. 
+          Get started by clicking the link on the home page “Find Homes to Sit” and you will then be redirected to the Trust My Pet Sitter key search page.
+        </p>
+        <h4>The Key Search Page</h4>
+        <p className={css.insideP}>
+          The Key Search Page allows you to select single or multiple options to find compatible Pet Sitters. You can search by:
+        </p>
+        <ul className={css.dotted}>
+          <li>
+            <strong>Location</strong> – in the top search bar locate the magnifying glass search icon and free type if you want to find pet sitters who are in a specific location. This is especially useful for finding local sitters, so for example typing “Edinburgh” would locate.
+          </li>
+          <li>
+            <strong>Category</strong> – Select the Category button from the top toolbar and choose to search for Pet Sitters. The results displayed will show your selection of Pet Sitters and if no selection is made then both categories will be displayed in the search results and location map.
+          </li>
+          <li>
+            <strong>Pet Types</strong> – You can choose to search by Pet Type, if none are chosen all Pet Types will be displayed in the search results and location map. Pet Types are Dog, Cat, Reptiles, Farm Animals, Rabbits, Pet Birds, Aquarium Fish, Horses or other.
+          </li>
+          <li>
+            <strong>Price Range</strong> – When Pet Sitters created their listing, they are asked to provide their price per night to look after your pets in their own home. You can choose to search Pet Sitters by their nightly rate by using the toggle to select the maximum nightly rate you are prepared to pay.
+          </li>
+
+          <li>
+            <strong>Dates</strong> – You can search for Pet Sitters who have expressed availability dates.
+          </li>
+        </ul>
+        <h3>Map Search</h3>
+        <ul className={css.dotted}>
+          <li>
+            You can also search for Pet Sitters who are in a specific map location. The map is shown on the right hand of the listing displays and Pet Sitters cost per night is shown on their location. For example, click on a price per night and the listing will pop up as a thumbnail image, if the Pet Sitter is of interest you can click on the image and it will load the main listing for you to browse suitability.
+          </li>
+          <li>
+            You can use the + and – buttons on the map to zoom in and out for more detailed searches.
+          </li>
+        </ul>
+      </div>,
+      <div>
+        <h3 className={css.insideTitle}>How are Pet Sitter and Owners verified?</h3>
+        <p className={css.insideP}>
+          From your payment details to your private conversations, all your information on our platform is encrypted, safe, and secure.
+        </p>
+        <h4>How are Pet Sitter and Owners verified?</h4>
+        <p className={css.insideP}>
+          All Pet Sitters and Pet Owners are encouraged to prove ID verification, offering all our users more peace of mind.
+        </p>
+        <p className={css.insideP}>We use Yoti.com to ID users, verifying ID from 130 countries within minutes.  You can see a community member has been verified by Yoti by the badge that will be displayed on all their listings.  Learn more about Yoti <NamedLink name="YotiPage">here</NamedLink>.</p>
+        <p className={css.insideP}>We encourage all users to undergo Yoti ID verification and regular prompts are provided. As a Pet Sitter or Pet Owner please consider if you choose a provider who has chosen not to undergo Yoti ID verification then they are not covered by our Emergency Insurance. The Yoti badge displayed on listings is your proof of ID verification.
+</p>
+        <h4>Yoti Verification Level:</h4>
+        <ul className={css.dotted}>
+          <li>
+            Photo
+          </li>
+
+          <li>
+            Full Name
+          </li>
+
+          <li>
+            Mobile Number
+          </li>
+          <li>
+            Email Address
+          </li>
+          <li>
+            Date of Birth
+          </li>
+          <li>
+            Gender
+          </li>
+          <li>
+            Nationality
+          </li>
+          <li>
+            Photo Authentication
+          </li>
+        </ul>
+      </div>,
     ];
 
     return (
