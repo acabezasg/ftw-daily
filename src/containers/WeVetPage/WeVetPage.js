045295f6da2c53962a204628816af04a63abcf56
AleksaPreb <aleksaprebiracevicorona@outlook.com>
2020-02-26 23:58:11 +0100

    changes

--- src/containers/WeVetPage/WeVetPage.js
+++ src/containers/WeVetPage/WeVetPage.js
@@ -72,6 +72,32 @@ const WeVetPage = () => {
           </div>
         </div>
 
+        
+        <div className={css.affiliateFirst}>
+          <div className={css.sectionContent}>
+            <h1>We can maybe add some title?</h1>
+            <h2>No1 Backup</h2>
+            <p>Choosing Pet Sitting as a career means professionals are familiar with a wide variety of pets and unlike non-professionals or family and friends, they have the proper experience to handle different situations. We back them up with Virtual Vet available 24/7 to answer questions and provide advice, in addition to our Property, Public Liability and Damage Insurance. They’ve also got a member of our team at the end of a phone when they need us.</p>
+            <h2>No2 Responsibility</h2>
+            <p>Professional Pet Sitters have a responsibility to look after their clients’ property and be on the front line, should an emergency occur. That’s why we provide an Insurance backed guarantee and Public Liability for up to £1m</p>
+            <h2>No3 Professional</h2>
+            <p>Some new clients are worried about letting a stranger into their home. This is a legitimate concern, but it’s good to keep in mind that Professional Pet Sitters are like Home Cleaners, Nanny’s, Real Estate Agents, Trade workers, and Contractors. It is an intricate part of their job to visit and enter people’s homes, and part of what they do every day. We provide the added peace of mind with Digital ID Verification and an Insurance backed guarantee for up to £1m. A Professional Pet Sitter will always be respectful of their clients’ property and belongings.</p>
+            <h2>No4 Personal Approval</h2>
+            <p>We don’t just rely on computer algorithms; a member of our team personally approves every Pet Sitter and we keep a close eye on reviews too.</p>
+            <h2>No5 Backup</h2>
+            <p>Some people still believe that asking friends, neighbours or a family member is a better option. The truth is, not a lot of people enjoy driving across town after work, on the weekends or on special days like Christmas to go scoop the litter box of someone else’s cat.</p>
+            <p>But let’s say you did manage to secure a friend or family member to care for your pets and are certain they won’t bail on you. You think your pets are in good hands. Are you sure?</p>
+            <p>Does your home insurance cover you for damages? Do they have 24/7 Vet Advice? Are they able to fulfil the schedule your pet needs?</p>
+            <p>Our Professional Pet Sitters come with all the advantages and none of the disadvantages. You tell them when you need them, what you need them to do, and you get peace of mind by taking advantage of our Insurance backed guarantees.</p>
+            <h2>No6 Insured</h2>
+            <p><strong><i>“If you think hiring a professional is expensive, wait until you hire an amateur”</i>, Red Adair</strong></p>
+            <p>If you are looking for the least expensive service you can find, then we are not for you. The girl next door, a close neighbour or a browse through Facebook you will certainly find a cheaper option.</p>
+            <p>You’ll get what you pay for, by way of peace of mind, professional quality service, experience and accountability. Our Professional Pet Sitters are fully insured and come with a £1m guarantee.</p>
+            <p>Have we been able to convince you to hire a Professional Pet Sitter?
+If not, tell us why! To us, there’s no substitute for the quality of pet care and peace of mind our clients receive by signing a professional. If you’re still hesitant, let us know, we’d love to talk to you about it.</p>
+          </div>
+        </div>
+
 
         <div className={css.affiliateFirst}>
           <div className={css.sectionContent}>
