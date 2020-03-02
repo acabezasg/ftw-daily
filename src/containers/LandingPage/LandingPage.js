2ff07b98cb92891270b4ee46f12b6368f5dffcb5
AleksaPreb <aleksaprebiracevicorona@outlook.com>
2020-03-03 00:53:54 +0100

    changes

--- src/containers/LandingPage/LandingPage.js
+++ src/containers/LandingPage/LandingPage.js
@@ -145,66 +145,55 @@ export const LandingPageComponent = props => {
 
             <li className={css.sections}>
               <div className={css.testimonialsSection}>
-                <h1>We Break Myths</h1>
+                <h1>We break Myths</h1>
                  <div className={css.testimonials}>
                    <div className={css.testimonial}>
-                     <p className={css.tContent}>Being a Pet Sitter love being my own boss. I am free to choose the location and dates that suit me. I get to meet amazing animals and their owners. I get to stay in some amazing locations and houses. The best job ever!</p>
+                     <p className={css.tContent}>I love being a Pet Sitter.  I am free to choose the location and dates that suit me. I get to meet amazing animals and their owners. I get to stay in some amazing locations and houses. The best job ever!</p>
                       <div className={css.testimonialAuthor}>
                        <img src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-grey-photo-placeholder-women-default-avatar-profile-icon-grey-photo-placeholder-illustrations-vectors-99724605.jpg" />
                        <span className={css.testR}>
                          <p className={css.smallAuthor}>
-                           Julie O'Reilly
-                         </p>
-                         <p>
-                           England
+                           Julie O - <span className={css.tLoc}>UK</span>
                          </p>
                        </span>
                      </div>
                    </div>
                    <div className={css.testimonial}>
-                     <p className={css.tContent}>There's many things I love about a pet sitter, I love getting to spend so much time with a variety of loving animals, getting to traing them, seeing the progress and grow. The relationship I build with them and their owners is lovely and very rewarding.</p>
-                      <div className={css.testimonialAuthor}>
-                       <img src="https://thumbs.dreamstime.com/b/default-placeholder-profile-icon-avatar-gray-man-90197971.jpg" />
-                       <span className={css.testR}>
-                         <p className={css.smallAuthor}>
-                           Holly Probert
-                         </p>
-                         <p>
-                           England
-                         </p>
-                       </span>
-                     </div>
-                   </div>
-                   <div className={css.testimonial}>
-                   <p className={css.tContent}>I love being a pet sitter because I always had pets and loved being surrounded by animals and having the chance to look after them. By being a pet sitter I get the opportunity to meet new furry and human friends. I know how important is to have someone reliable to look after your pets especially as they get access to your home and personal belongings.</p>
+                     <p className={css.tContent}>There's many things I love about a pet sitter, I love getting to spend so much time with a variety of loving pets.  The relationship I build with them and their owners is lovely and very rewarding.</p>
                       <div className={css.testimonialAuthor}>
                        <img src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-grey-photo-placeholder-women-default-avatar-profile-icon-grey-photo-placeholder-illustrations-vectors-99724605.jpg" />
                        <span className={css.testR}>
                          <p className={css.smallAuthor}>
-                          Paula-Alexandra
-                         </p>
-                         <p>
-                           England
+                           Holly P - <span className={css.tLoc}>UK</span>
                          </p>
                        </span>
                      </div>
                    </div>
                    <div className={css.testimonial}>
-                     <p className={css.tContent}>Get to know the animals while experiencing a different air is a heart warming experience that keeps me going. It ends up being your home too.</p>
+                   <p className={css.tContent}>I love being a pet sitter because I am surrounded by animals and enjoy the chance to look after them. Being a pet sitter gives me the opportunity to meet new furry and human friends. I know how important is to have someone reliable to look after your pets and I love what I do.</p>
+                      <div className={css.testimonialAuthor}>
+                       <img src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-grey-photo-placeholder-women-default-avatar-profile-icon-grey-photo-placeholder-illustrations-vectors-99724605.jpg" />
+                       <span className={css.testR}>
+                         <p className={css.smallAuthor}>
+                           Paula Alexandra - <span className={css.tLoc}>UK</span>
+                         </p>
+                       </span>
+                     </div>
+                   </div>
+                   <div className={css.testimonial}>
+                     <p className={css.tContent}>I love getting to know the pets and travelling to different locations.  When you watch a pet in their own home it ends up feeling like you are home too. I love being a Pet Sitter!</p>
                       <div className={css.testimonialAuthor}>
                        <img src="https://thumbs.dreamstime.com/b/default-placeholder-profile-icon-avatar-gray-man-90197971.jpg" />
                        <span className={css.testR}>
                          <p className={css.smallAuthor}>
-                           Sebasitan Di Giuseppe
-                         </p>
-                         <p>
-                           Argentina
+                           Sebastian D - <span className={css.tLoc}>Europe</span>
                          </p>
                        </span>
                      </div>
                    </div>
                  </div>
               </div>
+              <hr className={css.tHr} />
             </li>
 
             <li className={css.sections}>
