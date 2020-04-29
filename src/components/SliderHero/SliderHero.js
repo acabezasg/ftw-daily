e1d958c009770de0f9059fe67c0567af83847195
AleksaPreb <aleksaprebiracevicorona@outlook.com>
2020-04-29 06:37:09 +0200

    test2

--- src/components/SliderHero/SliderHero.js
+++ src/components/SliderHero/SliderHero.js
@@ -1,43 +1,53 @@
-import React from "react";
-import Carousel from "react-bootstrap/Carousel";
-import { render } from "react-dom";
-import { fadeInUp, fadeInLeft, fadeInRight } from "react-animations";
-import Radium, { StyleRoot } from "radium";
-import { NamedLink, ExternalLink } from "../../components";
+import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { render } from 'react-dom';
+import { fadeInUp, fadeInLeft, fadeInRight } from 'react-animations';
+import Radium, { StyleRoot } from 'radium';
+import { NamedLink, ExternalLink } from '../../components';
 
-import css from "./SliderHero.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import css from './SliderHero.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import forthSlide from "./slides/forth.jpg";
-import test2 from "./testslides/test2.jpg";
-import test5 from "./testslides/test5.jpg";
-import test6 from "./testslides/test6.jpg";
+import forthSlide from './slides/forth.jpg';
+import test2 from './testslides/test2.jpg';
+import test5 from './testslides/test5.jpg';
+import test6 from './testslides/test6.jpg';
+
+// Import demo slides
+
+import img1 from './test/img1.jpg';
+import img2 from './test/img2.jpg';
+import img3 from './test/img3.jpg';
+import img4 from './test/img4.jpg';
+import img5 from './test/img5.jpg';
+import img6 from './test/img6.jpg';
+import img7 from './test/img7.jpg';
+import img8 from './test/img8.jpg';
+import img9 from './test/img9.jpg';
 
 const styles = {
   fadeInUp: {
-    animation: "x 1s",
-    animationName: Radium.keyframes(fadeInUp, "fadeInUp")
-  }
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeInUp, 'fadeInUp'),
+  },
 };
 
 const stylesLeft = {
   fadeInLeft: {
-    animation: "x 1s",
-    animationName: Radium.keyframes(fadeInLeft, "fadeInLeft")
-  }
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeInLeft, 'fadeInLeft'),
+  },
 };
 
 const stylesRight = {
   fadeInRight: {
-    animation: "x 1s",
-    animationName: Radium.keyframes(fadeInRight, "fadeInRight")
-  }
+    animation: 'x 1s',
+    animationName: Radium.keyframes(fadeInRight, 'fadeInRight'),
+  },
 };
 
 const SliderHero = () => (
   <Carousel interval="8000" fade={true}>
-
-
     <Carousel.Item>
       <div className={css.carouselHero}>
         <img src={test5}></img>
@@ -52,10 +62,11 @@ const SliderHero = () => (
               <a href="s?pub_user_type=1">Find a Pet Sitter</a>
               <a href="s?pub_user_type=0">Find Homes to Sit</a>
               <a href="s?pub_user_type=2">Find a Pet Service</a>
-              <ExternalLink href="https://trustmypetsitter.seedrs.com/">We Are Crowdfunding on Seedrs</ExternalLink>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
             </div>
           </div>
-
         </StyleRoot>
       </div>
     </Carousel.Item>
@@ -75,10 +86,11 @@ const SliderHero = () => (
               <a href="s?pub_user_type=1">Find a Pet Sitter</a>
               <a href="s?pub_user_type=0">Find Homes to Sit</a>
               <a href="s?pub_user_type=2">Find a Pet Service</a>
-              <ExternalLink href="https://trustmypetsitter.seedrs.com/">We Are Crowdfunding on Seedrs</ExternalLink>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
             </div>
           </div>
-
         </StyleRoot>
       </div>
     </Carousel.Item>
@@ -97,10 +109,11 @@ const SliderHero = () => (
               <a href="s?pub_user_type=1">Find a Pet Sitter</a>
               <a href="s?pub_user_type=0">Find Homes to Sit</a>
               <a href="s?pub_user_type=2">Find a Pet Service</a>
-              <ExternalLink href="https://trustmypetsitter.seedrs.com/">We Are Crowdfunding on Seedrs</ExternalLink>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
             </div>
           </div>
-
         </StyleRoot>
       </div>
     </Carousel.Item>
@@ -119,14 +132,221 @@ const SliderHero = () => (
               <a href="s?pub_user_type=1">Find a Pet Sitter</a>
               <a href="s?pub_user_type=0">Find Homes to Sit</a>
               <a href="s?pub_user_type=2">Find a Pet Service</a>
-              <ExternalLink href="https://trustmypetsitter.seedrs.com/">We Are Crowdfunding on Seedrs</ExternalLink>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
             </div>
           </div>
-
         </StyleRoot>
       </div>
     </Carousel.Item>
 
+    <Carousel.Item>
+      <div className={css.carouselHero}>
+        <img src={img1}></img>
+      </div>
+
+      <div className={css.carouselCaption}>
+        <StyleRoot>
+          <div style={styles.fadeInUp}>
+            <h1 style={styles.fadeInUp}>Stay in wonderful locations</h1>
+
+            <div className={css.submitContainer}>
+              <a href="s?pub_user_type=1">Find a Pet Sitter</a>
+              <a href="s?pub_user_type=0">Find Homes to Sit</a>
+              <a href="s?pub_user_type=2">Find a Pet Service</a>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
+            </div>
+          </div>
+        </StyleRoot>
+      </div>
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <div className={css.carouselHero}>
+        <img src={img2}></img>
+      </div>
+
+      <div className={css.carouselCaption}>
+        <StyleRoot>
+          <div style={styles.fadeInUp}>
+            <h1 style={styles.fadeInUp}>Stay in wonderful locations</h1>
+
+            <div className={css.submitContainer}>
+              <a href="s?pub_user_type=1">Find a Pet Sitter</a>
+              <a href="s?pub_user_type=0">Find Homes to Sit</a>
+              <a href="s?pub_user_type=2">Find a Pet Service</a>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
+            </div>
+          </div>
+        </StyleRoot>
+      </div>
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <div className={css.carouselHero}>
+        <img src={img3}></img>
+      </div>
+
+      <div className={css.carouselCaption}>
+        <StyleRoot>
+          <div style={styles.fadeInUp}>
+            <h1 style={styles.fadeInUp}>Stay in wonderful locations</h1>
+
+            <div className={css.submitContainer}>
+              <a href="s?pub_user_type=1">Find a Pet Sitter</a>
+              <a href="s?pub_user_type=0">Find Homes to Sit</a>
+              <a href="s?pub_user_type=2">Find a Pet Service</a>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
+            </div>
+          </div>
+        </StyleRoot>
+      </div>
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <div className={css.carouselHero}>
+        <img src={img4}></img>
+      </div>
+
+      <div className={css.carouselCaption}>
+        <StyleRoot>
+          <div style={styles.fadeInUp}>
+            <h1 style={styles.fadeInUp}>Stay in wonderful locations</h1>
+
+            <div className={css.submitContainer}>
+              <a href="s?pub_user_type=1">Find a Pet Sitter</a>
+              <a href="s?pub_user_type=0">Find Homes to Sit</a>
+              <a href="s?pub_user_type=2">Find a Pet Service</a>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
+            </div>
+          </div>
+        </StyleRoot>
+      </div>
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <div className={css.carouselHero}>
+        <img src={img5}></img>
+      </div>
+
+      <div className={css.carouselCaption}>
+        <StyleRoot>
+          <div style={styles.fadeInUp}>
+            <h1 style={styles.fadeInUp}>Stay in wonderful locations</h1>
+
+            <div className={css.submitContainer}>
+              <a href="s?pub_user_type=1">Find a Pet Sitter</a>
+              <a href="s?pub_user_type=0">Find Homes to Sit</a>
+              <a href="s?pub_user_type=2">Find a Pet Service</a>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
+            </div>
+          </div>
+        </StyleRoot>
+      </div>
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <div className={css.carouselHero}>
+        <img src={img6}></img>
+      </div>
+
+      <div className={css.carouselCaption}>
+        <StyleRoot>
+          <div style={styles.fadeInUp}>
+            <h1 style={styles.fadeInUp}>Stay in wonderful locations</h1>
+
+            <div className={css.submitContainer}>
+              <a href="s?pub_user_type=1">Find a Pet Sitter</a>
+              <a href="s?pub_user_type=0">Find Homes to Sit</a>
+              <a href="s?pub_user_type=2">Find a Pet Service</a>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
+            </div>
+          </div>
+        </StyleRoot>
+      </div>
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <div className={css.carouselHero}>
+        <img src={img7}></img>
+      </div>
+
+      <div className={css.carouselCaption}>
+        <StyleRoot>
+          <div style={styles.fadeInUp}>
+            <h1 style={styles.fadeInUp}>Stay in wonderful locations</h1>
+
+            <div className={css.submitContainer}>
+              <a href="s?pub_user_type=1">Find a Pet Sitter</a>
+              <a href="s?pub_user_type=0">Find Homes to Sit</a>
+              <a href="s?pub_user_type=2">Find a Pet Service</a>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
+            </div>
+          </div>
+        </StyleRoot>
+      </div>
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <div className={css.carouselHero}>
+        <img src={img8}></img>
+      </div>
+
+      <div className={css.carouselCaption}>
+        <StyleRoot>
+          <div style={styles.fadeInUp}>
+            <h1 style={styles.fadeInUp}>Stay in wonderful locations</h1>
+
+            <div className={css.submitContainer}>
+              <a href="s?pub_user_type=1">Find a Pet Sitter</a>
+              <a href="s?pub_user_type=0">Find Homes to Sit</a>
+              <a href="s?pub_user_type=2">Find a Pet Service</a>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
+            </div>
+          </div>
+        </StyleRoot>
+      </div>
+    </Carousel.Item>
+
+    <Carousel.Item>
+      <div className={css.carouselHero}>
+        <img src={img9}></img>
+      </div>
+
+      <div className={css.carouselCaption}>
+        <StyleRoot>
+          <div style={styles.fadeInUp}>
+            <h1 style={styles.fadeInUp}>Stay in wonderful locations</h1>
+
+            <div className={css.submitContainer}>
+              <a href="s?pub_user_type=1">Find a Pet Sitter</a>
+              <a href="s?pub_user_type=0">Find Homes to Sit</a>
+              <a href="s?pub_user_type=2">Find a Pet Service</a>
+              <ExternalLink href="https://trustmypetsitter.seedrs.com/">
+                We Are Crowdfunding on Seedrs
+              </ExternalLink>
+            </div>
+          </div>
+        </StyleRoot>
+      </div>
+    </Carousel.Item>
   </Carousel>
 );
 
